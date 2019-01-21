ee76d7b3b5a0d0603302b7b83f70e1549fb8e88b
thomasjlew <lewt@student.ethz.ch>
2019-01-20 22:31:22 -0800

    Fixed car definition causing bug with Bullet.

--- src/robot/car.jl
+++ src/robot/car.jl
@@ -3,4 +3,6 @@ export Car
 mutable struct Car <: Robot 
   btCollisionObject
 end
-Car() = Car(BulletCollision.convex_hull([zeros(3)]))
+# Necessary - Avoids zero distance when calling Bullet
+#Car() = Car(BulletCollision.convex_hull([zeros(3)]))
+Car() = Car(BulletCollision.sphere(SVector{3}(zeros(Float32,3)), 0.001))
